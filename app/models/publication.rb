e3ce442f4a6a308eb9ac530fc855f9f00dc739aa
Tekin Suleyman <tekin@tekin.co.uk>
2013-09-27 12:46:00 +0100

    Remove unused method

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -103,10 +103,6 @@ class Publication < Publicationesque
 
   private
 
-  def scheduled_or_published?
-    published? || scheduled?
-  end
-
   def attachment_required_before_moving_out_of_draft
     if %w(submitted scheduled published).include?(state) && !has_attachments?
       errors.add(:base, "cannot be #{current_state} without an attachment or HTML version")
