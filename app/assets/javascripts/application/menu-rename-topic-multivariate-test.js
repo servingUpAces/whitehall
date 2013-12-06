3b39dcd1c36ea6e93e8b2eed27a2dd41a11ee694
Ben Firshman <ben@firshman.co.uk>
2013-12-06 12:12:25 +0000

    Add multivariate test for menu topic text
    https://www.pivotaltracker.com/s/projects/367813/stories/61474462

---
+++ app/assets/javascripts/application/menu-rename-topic-multivariate-test.js
@@ -0,0 +1,26 @@
+(function() {
+  "use strict";
+  window.GOVUK = window.GOVUK || {};
+
+  function MenuRenameTopicMultivariateTest(options) {
+    GOVUK.MultivariateTest.call(this, options);
+  }
+
+  MenuRenameTopicMultivariateTest.prototype = Object.create(GOVUK.MultivariateTest.prototype);
+
+  MenuRenameTopicMultivariateTest.prototype.name = 'topics_menu_text';
+  MenuRenameTopicMultivariateTest.prototype.customVarIndex = 14;
+  MenuRenameTopicMultivariateTest.prototype.cohorts = {
+    topics: {},
+    policy_areas: {
+      callback: "policyAreasCallback"
+    }
+  };
+
+  MenuRenameTopicMultivariateTest.prototype.policyAreasCallback = function() {
+    this.$el.find('.js-topics-link').text('Policy areas');
+    this.$el.find('.js-policies-link').hide();
+  }
+  GOVUK.MenuRenameTopicMultivariateTest = MenuRenameTopicMultivariateTest;
+})();
+
